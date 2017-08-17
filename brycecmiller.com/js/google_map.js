976a5b063a6f018de89064c0c923bcb88c68154a
bcmiller713 <bcmiller713@gmail.com>
2017-08-16 22:49:45 -0700

    updates to text content, added project screenshots, made portfolio section more responsive

--- brycecmiller.com/js/google_map.js
+++ brycecmiller.com/js/google_map.js
@@ -25,25 +25,25 @@ function init() {
 
     // Get the HTML DOM element that will contain your map 
     // We are using a div with id="map" seen below in the <body>
-    var mapElement = document.getElementById('map');
+    var mapElement = document.getElementById("map");
 
     // Create the Google Map using out element and options defined above
     var map = new google.maps.Map(mapElement, mapOptions);
     
-    var addresses = ['university+city+san+diego'];
+    var addresses = ["university+city+san+diego"];
 
     for (var x = 0; x < addresses.length; x++) {
-        $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
+        $.getJSON("http://maps.googleapis.com/maps/api/geocode/json?address="+addresses[x], null, function (data) {
             var p = data.results[0].geometry.location
             var latlng = new google.maps.LatLng(p.lat, p.lng);
             new google.maps.Marker({
                 position: latlng,
                 map: map,
-                icon: 'images/loc.png'
+                icon: "images/location.png"
             });
 
         });
     }
     
 }
-google.maps.event.addDomListener(window, 'load', init);
+google.maps.event.addDomListener(window, "load", init);
